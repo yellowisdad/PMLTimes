08d304e8c1e643cb1d35d8302d37b1ed5596746d
Methawee <methawee.pun@truedigital.com>
2022-03-08 01:30:33 +0700

    reactor : remove pod project

--- Pods/Target Support Files/Pods-PMLTimes-PMLTimesUITests/Pods-PMLTimes-PMLTimesUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PMLTimes_PMLTimesUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PMLTimes_PMLTimesUITests
-@end
--- Pods/Target Support Files/Pods-PMLTimes/Pods-PMLTimes-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PMLTimes : NSObject
-@end
-@implementation PodsDummy_Pods_PMLTimes
-@end
