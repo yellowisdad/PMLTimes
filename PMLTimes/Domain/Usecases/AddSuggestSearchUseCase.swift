c9660396239ae6dce216da482619997d8ebbe7f8
Methawee <methawee.pun@truedigital.com>
2022-03-07 17:34:13 +0700

    feat : search suggest from FirebaseFirestore

--- PMLTimes/Domain/Usecases/AddSuggestSearchUseCase.swift
+++ PMLTimes/Domain/Usecases/AddSuggestSearchUseCase.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import RxSwift
+import FirebaseFirestore
+
+final class AddSuggestSearchUseCase: UseCase {
+    
+    fileprivate var db = Firestore.firestore()
+    init() {}
+    
+    public func execute(_ query: [String]) {
+        let document = db.collection("pmlTimes").document("b0Te2FroxTf2MIlRvWy1")
+        document.updateData([
+            "suggest-search": query
+        ]) { error in
+            print(error?.localizedDescription ?? "")
+        }
+    }
+    
+//    public func execute(_ param: (query: String, completion: ((Error?)->Void))) {
+//        let document = db.collection("pmlTimes").document("JqmosZzfMPsa0UuSnoFx")
+//        document.updateData([
+//            "suggest-search": FieldValue.arrayUnion([param.query])
+//        ]) { error in
+//            param.completion(error)
+//        }
+//    }
+}
+
+
--- PMLTimes/Domain/Usecases/SuggestSearchUseCase.swift
+++ PMLTimes/Domain/Usecases/SuggestSearchUseCase.swift
@@ -28,28 +28,3 @@ final class SuggestSearchUseCase: UseCase {
     }
 }
 
-
-final class AddSuggestSearchUseCase: UseCase {
-    
-    fileprivate var db = Firestore.firestore()
-    init() {}
-    
-    public func execute(_ query: [String]) {
-        let document = db.collection("pmlTimes").document("b0Te2FroxTf2MIlRvWy1")
-        document.updateData([
-            "suggest-search": query
-        ]) { error in
-            print(error?.localizedDescription ?? "")
-        }
-    }
-    
-//    public func execute(_ param: (query: String, completion: ((Error?)->Void))) {
-//        let document = db.collection("pmlTimes").document("JqmosZzfMPsa0UuSnoFx")
-//        document.updateData([
-//            "suggest-search": FieldValue.arrayUnion([param.query])
-//        ]) { error in
-//            param.completion(error)
-//        }
-//    }
-}
-
